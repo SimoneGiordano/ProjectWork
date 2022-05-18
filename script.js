cdceea342cfb62ac5f3d4f385945f5c836ca8492
Simone <giordanosimone.98@gmail.com>
2022-05-19 00:58:37 +0200

    wip

--- script.js
+++ script.js
@@ -23,9 +23,6 @@ function myDelete() {
     }
   }
 
-
-
-
 // add to wish list
 
 const buttonAddToWishList = document.querySelectorAll(".add-to-list");
@@ -79,3 +76,36 @@ function inizializzaWishlist() {
 }
 
 inizializzaWishlist();
+
+
+
+// carousel product page
+
+const slider = document.querySelector('.gallery');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', e => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', _ => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', _ => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', e => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const SCROLL_SPEED = 3;
+  const walk = (x - startX) * SCROLL_SPEED;
+  slider.scrollLeft = scrollLeft - walk;
+});
+
